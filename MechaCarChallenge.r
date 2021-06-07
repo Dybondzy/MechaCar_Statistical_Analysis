17d00230a1f1fb0c2b3003c5859e544ead4096fd
Dybondzy <79281335+Dybondzy@users.noreply.github.com>
2021-06-07 15:35:26 -0400

    Add files via upload

--- MechaCarChallenge.r
+++ MechaCarChallenge.r
@@ -9,34 +9,28 @@ head(MechaCar_mpg)
 
 
 # Perform linear regression using the lm() function. In the lm() function, pass in all six variables (i.e., columns), and add the dataframe you created in Step 4 as the data parameter.
-Mech_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data =  mecha_data)
-
+Mecha_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data =  mecha_data)
+head(Mecha_lm)
 
 # Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(Mecha_lm)
 
 
-# Save your MechaCarChallenge.RScript file to your GitHub repository.
-
-
-
 # Download the Suspension_Coil.csv file, and place it in the active directory for your R session.
-Suspension_coil <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
-
+suspension_data <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+head(suspension_data)
 
 #bIn your MechaCarChallenge.RScript, import and read in the Suspension_Coil.csv file as a table.
-total_summary <- Suspension_coil %>%
-  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
-
 
 # Write an RScript that creates a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil's PSI column.
-total_summary <- Suspension_coil %>% group_by(Manufacturing_Lot) %>%
-  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
-  
-
+total_summary <- suspension_data %>% 
+  summarise(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
 
 # Your total_summary dataframe should look like this:
+total_summaryG <- suspension_data %>% group_by(Manufacturing_Lot) %>%
+  summarise(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
 # Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI column.
 
 # Perform t-test across all lots
@@ -54,8 +48,4 @@ t.test(subset(suspension_data,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
 # Perform t-test on Lot 3
 t.test(subset(suspension_data,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
 
-
-
-# Your lot_summary dataframe should look like this:
-# Save your MechaCarChallenge.RScript file to your GitHub repository.
 
